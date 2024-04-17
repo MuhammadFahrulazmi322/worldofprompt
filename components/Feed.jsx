6e6f66b219dff461f99f4c5a5b1e4c10253e4a29
MuhammadFahrulazmi322 <fazmi322@gmail.com>
2024-04-17 16:20:45 +0700

    feed jsx with input search

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
 
 const Feed = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
-    <div>Feed</div>
+    <div className="mt-16 w-full lg:w-1/2">
+      <input 
+        type="text" 
+        placeholder="Search for tag or a username" 
+        value={searchTerm} 
+        onChange={(e) => setSearchTerm(e.target.value)} 
+        className="w-full text-left p-3 border-1 border-gray-300 rounded-lg shadow-lg shadow-primary-color/20"
+      />
+    </div>
   )
 }
 
