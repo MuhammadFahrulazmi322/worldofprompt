e49fcb07035a05a6da3c0b1ac8800978274f81ad
MuhammadFahrulazmi322 <fazmi322@gmail.com>
2024-05-07 13:28:49 +0700

    update PromptCardList

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -1,54 +1,49 @@
 import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
 
-export const GET = async (request, { params }) => {
-    try {
-        await connectToDB()
+// GET (read specified data)
 
-        const prompt = await Prompt.findById(params.id).populate("creator")
-        if (!prompt) return new Response("Prompt Not Found", { status: 404 });
-
-        return new Response(JSON.stringify(prompt), { status: 200 })
-
-    } catch (error) {
-        return new Response("Internal Server Error", { status: 500 });
-    }
-}
-
-export const PATCH = async (request, { params }) => {
-    const { prompt, tag } = await request.json();
-
+export const GET = async (req, { params }) => {
     try {
         await connectToDB();
+        const prompts = await Prompt.findById(params.id).populate("creator");
+        if (!prompts) {
+            return new Response("Prompt not found", { status: 404 });
+        }
+        return new Response(JSON.stringify(prompts), { status: 200 });
+    } catch (error) {
+        return new Response("Failed to fetch all prompts", { status: 500 });
+    }
+};
 
-        // Find the existing prompt by ID
+// Patch (Updated)
+export const PATCH = async (req, { params }) => {
+    const { prompt, tag } = await req.json();
+    try {
+        await connectToDB();
         const existingPrompt = await Prompt.findById(params.id);
-
         if (!existingPrompt) {
             return new Response("Prompt not found", { status: 404 });
         }
-
-        // Update the prompt with new data
         existingPrompt.prompt = prompt;
         existingPrompt.tag = tag;
-
         await existingPrompt.save();
-
-        return new Response("Successfully updated the Prompts", { status: 200 });
+        return new Response(JSON.stringify(existingPrompt), { status: 200 });
     } catch (error) {
-        return new Response("Error Updating Prompt", { status: 500 });
+        return new Response("Failed to update prompt", { status: 500 });
     }
-};
 
-export const DELETE = async (request, { params }) => {
+}
+
+
+// DELETE (delete)
+export const DELETE = async (req, { params }) => {
     try {
         await connectToDB();
-
-        // Find the prompt by ID and remove it
-        await Prompt.findByIdAndRemove(params.id);
-
+        await Prompt.findByIdAndDelete(params.id);
         return new Response("Prompt deleted successfully", { status: 200 });
     } catch (error) {
-        return new Response("Error deleting prompt", { status: 500 });
+        return new Response("Failed to delete prompt", { status: 500 });
     }
-};
+
+}
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,28 +1,13 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState} from "react";
 
-import PromptCard from "./PromptCard";
 import useResources from "@hooks/useResources";
-import Loading from "@app/loading";
+import PromptCardList from "./PromptCardList";
 
-const PromptCardList = ({ data, handleTagClick }) => {
-  return (
-    <div className="mt-16 prompt_layout">
-      {data.map((post) => (
-        <PromptCard
-          key={post._id}
-          post={post}
-          handleTagClick={handleTagClick}
-        />
-      ))}
-    </div>
-  );
-};
 
-const Feed = ({ refresh }) => {
+const Feed = () => {
   const allPosts = useResources();
-  const [loading, setLoading] = useState(false);
   // Search states
   const [searchText, setSearchText] = useState("");
   const [searchTimeout, setSearchTimeout] = useState(null);
