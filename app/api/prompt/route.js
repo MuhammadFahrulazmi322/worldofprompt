ff516c125a09ecf75c79e837723afa64279b1974
MuhammadFahrulazmi322 <fazmi322@gmail.com>
2024-05-07 15:38:14 +0700

    add no cache

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -6,8 +6,11 @@ export const GET = async (request) => {
         await connectToDB()
 
         const prompts = await Prompt.find({}).populate('creator')
+        const headers = {
+            "Cache-Control": "no-cache" // Disable caching
+        };
 
-        return new Response(JSON.stringify(prompts), { status: 200 })
+        return new Response(JSON.stringify(prompts), { status: 200, headers });
     } catch (error) {
         return new Response("Failed to fetch all prompts", { status: 500 })
     }
