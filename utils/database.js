73e34176400a0ed6cf5a1fed0f5b4a83eab87610
MuhammadFahrulazmi322 <fazmi322@gmail.com>
2024-05-24 15:55:53 +0700

    check log database

--- utils/database.js
+++ utils/database.js
@@ -1,24 +1,27 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"; // Import mongoose library
 
-let isConnected = false;
+let isConnected = false; // Flag to check if the database connection is already established
 
 export const connectToDB = async () => {
-  mongoose.set("strictQuery", true);
+  // Asynchronous function to connect to the database
+  mongoose.set("strictQuery", true); // Enable strict mode for query filters
 
   if (isConnected) {
-    console.log("MongoDB is already connected");
-    return;
+    // Check if the database is already connected
+    console.log("MongoDB is already connected"); // Log message if the database is already connected
+    return; // Exit the function if the database is already connected
   }
 
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "worldofprompt",
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      // Attempt to connect to the MongoDB database
+      dbName: "worldofprompt", // Specify the database name
     });
-    isConnected = true;
-    console.log("MongoDB connected");
+    isConnected = true; // Set the isConnected flag to true upon successful connection
+    console.log("MongoDB connected"); // Log message upon successful connection
+    console.log(`Connected to DB: ${process.env.MONGODB_URI}`); // Log the database URI for debugging
   } catch (error) {
-    console.log("MongoDB connection error:", error);
+    // Catch block to handle any errors during connection
+    console.log(error); // Log the error if the connection fails
   }
 };
